e3fc38724fff9238042fe909ca703247939f3d7c
Dengfeng Liu <liudf0716@gmail.com>
2021-11-30 14:54:03 +0800

    Update ssh2.h

--- ssh2.h
+++ ssh2.h
@@ -1,23 +1,25 @@
- class Ssh2
+#include <string>
+
+class Ssh2
  {
  public:
-  Ssh2(const string &srvIp, int srvPort=22 );
+  Ssh2(const std::string &srvIp, int srvPort=22 );
   ~Ssh2(void);
 
-  bool Connect( const string &userName, const string &userPwd);
+  bool Connect( const std::string &userName, const std::string &userPwd);
   bool Disconnect(void);
 
-  Channel* CreateChannel(const string &ptyType = "vanilla");
+  Channel* CreateChannel(const std::string &ptyType = "vanilla");
 
 public:
   static void S_KbdCallback(const char*, int, const char*, int, int, const LIBSSH2_USERAUTH_KBDINT_PROMPT*, LIBSSH2_USERAUTH_KBDINT_RESPONSE*, void **a );
-  static string s_password;
+  static std::string s_password;
 
 private:
-  string m_srvIp;
+  std::string m_srvIp;
   int    m_srvPort;
-  string m_userName;
-  string m_password;
+  std::string m_userName;
+  std::string m_password;
   int    m_sock;
   LIBSSH2_SESSION *m_session;
   };
